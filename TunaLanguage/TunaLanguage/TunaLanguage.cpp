5c5806f1ccec8be597880063e1a945668a9ef4be
Tturna <42873350+Tturna@users.noreply.github.com>
2021-11-03 00:39:36 +0200

    Initial VS C++ CMake Project

---
+++ TunaLanguage/TunaLanguage/TunaLanguage.cpp
@@ -0,0 +1,12 @@
+﻿// TunaLanguage.cpp : Defines the entry point for the application.
+//
+
+#include "TunaLanguage.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
